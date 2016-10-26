a155eede1045cdd6bfc7af0a74ed94484b861a51
Connor Shea <connor.james.shea@gmail.com>
2016-10-26 01:02:02 -0600

    Test raising errors for Rails deprecations.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -28,7 +28,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :test
 
   # Print deprecation notices to the stderr
-  config.active_support.deprecation = :stderr
+  config.active_support.deprecation = :raise
 
   config.eager_load = false
 
