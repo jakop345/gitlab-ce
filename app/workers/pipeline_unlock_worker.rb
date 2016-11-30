ec56a08d07b27387dcab9045246443eddee3960b
Grzegorz Bizon <grzesiek.bizon@gmail.com>
2016-11-30 15:09:46 +0100

    Split pipeline unlock worker perform implementation

--- app/workers/pipeline_unlock_worker.rb
+++ app/workers/pipeline_unlock_worker.rb
@@ -3,6 +3,13 @@ class PipelineUnlockWorker
   include CronjobQueue
 
   def perform
+    process_stuck_pipelines
+    finish_outdated_pipelines
+  end
+
+  private
+
+  def process_stuck_pipelines
     Ci::Pipeline.unfinished.with_builds
       .where('ci_commits.updated_at < ?', 6.hours.ago)
       .where('ci_commits.created_at > ?', 1.week.ago)
@@ -12,10 +19,12 @@ class PipelineUnlockWorker
         Sidekiq::Client.push_bulk('class' => PipelineProcessWorker,
                                   'args' => ids.in_groups_of(1))
       end
+  end
 
+  def finish_outdated_pipelines
     Ci::Pipeline.unfinished
       .where('ci_commits.created_at < ?', 1.week.ago)
-      .pluck(:id).tap do |ids|
+      .order(:id).pluck(:id).tap do |ids|
         break if ids.empty?
 
         Sidekiq::Client.push_bulk('class' => PipelineConcludeWorker,
