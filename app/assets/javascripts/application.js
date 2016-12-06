3d65263aa1dd2a6aa0e138fc0708fec76c778d59
Connor Shea <connor.james.shea@gmail.com>
2016-12-06 16:19:16 -0700

    Fix some of the issues with the jQuery UI update.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,11 +6,11 @@
 // the compiled file.
 //
 /*= require jquery2 */
-/*= require jquery-ui/autocomplete */
-/*= require jquery-ui/datepicker */
-/*= require jquery-ui/draggable */
-/*= require jquery-ui/effect-highlight */
-/*= require jquery-ui/sortable */
+/*= require jquery-ui/widgets/autocomplete */
+/*= require jquery-ui/widgets/datepicker */
+/*= require jquery-ui/widgets/draggable */
+/*= require jquery-ui/effects/effect-highlight */
+/*= require jquery-ui/widgets/sortable */
 /*= require jquery_ujs */
 /*= require jquery.endless-scroll */
 /*= require jquery.highlight */
